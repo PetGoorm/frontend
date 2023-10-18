cfba82dfe25f2f295ec6f5d0b05a6b5567348a32
joo <jooh9992@gmail.com>
2023-10-18 19:26:59 +0900

    (feat: 반려일지 수정)

--- src/pages/DiaryPage.tsx
+++ src/pages/DiaryPage.tsx
@@ -1,8 +1,12 @@
 import {Diary }from "../components/diary/Diary";
+import { Helmet } from 'react-helmet-async';
 
 export default function DiaryPage() {
     return (
         <>
+            <Helmet>
+                <title> 반려일지 | 펫구름 </title>
+            </Helmet>
             <Diary />
         </>
     )
