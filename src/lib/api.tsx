72af1fac7ede30cc8a82dd7052935f40e10e7532
Lee JooHyung <jooh9992@gmail.com>
2023-11-02 00:26:32 +0900

    (fix: Base URL 변경)

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-	baseURL : 'http://43.202.220.69:7777',
+	baseURL : 'https://petgoorm.life',
     headers: {
         "Content-Type": `application/json`,
       },
