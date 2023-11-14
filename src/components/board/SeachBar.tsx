295aace80de053621a6a28038edf2e95edeb9414
Minseo Oh <113974911+dongkiid@users.noreply.github.com>
2023-11-14 20:06:47 +0900

    (fix: 게시판 엔터로 검색 안되는 오류 수정, 게시물 댓글 컴포넌트 누락사항 재반영) (#26)
    * (fix: 게시판 엔터로 검색 안되는 오류 수정)
    
    * (fix:게시물 댓글 컴포넌트 재반영)

--- src/components/board/SeachBar.tsx
+++ src/components/board/SeachBar.tsx
@@ -5,39 +5,49 @@ import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface AreaProps {
   setKeyword: (keyword: string) => void;
   setSearch: (search: string) => void;
+  setSelectedCategory: string;
 }
 
 interface FormData {
   search: string,
-  keyword: string
+  keyword: string,
+  category: string
 }
 
-function SearchBar({ setKeyword, setSearch }: AreaProps) {
+function SearchBar({ setKeyword, setSearch, setSelectedCategory }: AreaProps) {
 
-  const [formData, setFormData] = useState<FormData>({ search: "제목", keyword: "" });
+  const [formData, setFormData] = useState<FormData>({ search: "제목", keyword: "", category: setSelectedCategory });
+
+  const navigate = useNavigate()
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
   }
 
-  const handleSearch = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
+  const handleSearch = (e: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => {
+    if (e.type === 'click' || (e.type === 'keydown' && (e as React.KeyboardEvent<HTMLElement>).key === 'Enter')) {
+      e.preventDefault();
+    
 
     const trimmedKeyword = formData.keyword.trim();
 
     if(trimmedKeyword && formData.search){
       setKeyword(trimmedKeyword);
       setSearch(formData.search);
+      
       console.log("formData.keyword " + formData.keyword);
+      navigate(`/board/list/${setSelectedCategory}?page=1&keyword=${trimmedKeyword}&search=${formData.search}`);
     } else {
       alert('검색어를 입력해주세요')
     }  
   }
+}
 
   return (
     <div style={{ margin: 20, padding: 20 }}>
@@ -51,7 +61,7 @@ function SearchBar({ setKeyword, setSearch }: AreaProps) {
         autoComplete="off"
       >
         <FormControl sx={{ m: 1, minWidth: 10 }}>
-          <NativeSelect
+          <NativeSelect 
             defaultValue={"none"}
             inputProps={{
               name: "search",
@@ -62,7 +72,7 @@ function SearchBar({ setKeyword, setSearch }: AreaProps) {
             <option value={"plus"}>제목+내용</option>
           </NativeSelect>
         </FormControl>
-        <TextField id="keyword" type="search" variant="standard" name="keyword" onChange={handleInputChange}/>
+        <TextField id="keyword" type="search" variant="standard" name="keyword" onChange={handleInputChange} onKeyDown={handleSearch}/>
         <IconButton sx={{ p: "10px" }} aria-label="search" onClick={handleSearch} >
           <SearchIcon />
         </IconButton>
